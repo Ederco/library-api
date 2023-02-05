46dc874b29b8c11b8251e12ca44d4d69b8163945
Eder Correia <ederco77@gmail.com>
2023-02-05 17:52:13 -0300

    Implementando o Travis

--- src/main/java/com/ederco/libraryapi/api/resource/BookController.java
+++ src/main/java/com/ederco/libraryapi/api/resource/BookController.java
@@ -6,8 +6,12 @@ import com.ederco.libraryapi.model.entity.Book;
 import com.ederco.libraryapi.model.entity.Loan;
 import com.ederco.libraryapi.service.BookService;
 import com.ederco.libraryapi.service.LoanService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -21,12 +25,19 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
-@RequiredArgsConstructor
+@Api("Book API")
+@Slf4j
 public class BookController {
 
     private final BookService service;
-    private final LoanService loanService;
     private final ModelMapper modelMapper;
+    private final LoanService loanService;
+
+    public BookController(BookService service, ModelMapper modelMapper, LoanService loanService) {
+        this.service = service;
+        this.modelMapper = modelMapper;
+        this.loanService = loanService;
+    }
 
 //    Com o uso do @RequiredArgsConstructor não é necessário o uso do construtor ...
 //    public BookController(BookService service, ModelMapper modelMapper, LoanService loanService) {
@@ -37,7 +48,9 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Create a book")
     public BookDTO create(@RequestBody @Valid BookDTO dto){
+        log.info(" creating a book for isbn: {} ", dto.getIsbn());
 //        Sem ModelMapper
 //        Book entity = Book
 //                .builder()
@@ -72,10 +85,12 @@ public class BookController {
 
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Obtains a book details by id")
     //O @PathVariable reverencia a variável "{id}" do path que
     //constrói a URL injetando o PathVariable que tem o mesmo nome,
     //neste caso o id do tipo Long
     public BookDTO get(@PathVariable Long id){
+        log.info(" obtaining details for book id: {} ", id);
         //O método "map" mapeia o resultado da consulta do "getById"
         return service
                 .getById(id)
@@ -84,7 +99,12 @@ public class BookController {
     }
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete a book by id")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Book succesfully deleted")
+    })
     public void delete(@PathVariable Long id){
+        log.info(" deleting book of id: {} ", id);
         //O getById retorna um Optional por isso utilizamos o .get na sequência
         //Book book = service.getById(id).get(); "Esta linha apresentou o erro NoSuchElementException",
         //por isso  , em lugar do .get utilizamos o .orElseThrow
@@ -93,9 +113,11 @@ public class BookController {
         //Neste momento será necessário criar o método delete na interface de service
     }
     @PutMapping("{id}")
+    @ApiOperation("Updates a book")
     //Por default retorna status 200 OK..
     //@ResponseStatus(HttpStatus.OK)
     public BookDTO update( @PathVariable Long id , BookDTO dto){
+        log.info(" updating book of id: {} ", id);
         //Podemos usar como base o método delete...
         return service.getById(id).map(book -> {
 
@@ -108,6 +130,7 @@ public class BookController {
 
     }
     @GetMapping
+    @ApiOperation("Lists books by params")
     public Page<BookDTO> find(BookDTO dto, Pageable pageRequest){
         Book filter = modelMapper.map(dto, Book.class);
         //**aqui havia apenas uma página de book "Page<Book>"
